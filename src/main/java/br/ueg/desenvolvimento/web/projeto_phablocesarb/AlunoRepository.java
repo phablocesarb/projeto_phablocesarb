f5da32dddb5e0c89dab6ece51c4ac0486a667d57
phablocesarb <phablocesar@gmail.com>
2025-04-22 14:42:50 -0300

    v3.0

--- src/main/java/br/ueg/desenvolvimento/web/projeto_phablocesarb/AlunoRepository.java
+++ src/main/java/br/ueg/desenvolvimento/web/projeto_phablocesarb/AlunoRepository.java
@@ -1,6 +1,9 @@
 package br.ueg.desenvolvimento.web.projeto_phablocesarb;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
+
+    List<Aluno> findByNomeContainingIgnoreCase(String nome);
 }
